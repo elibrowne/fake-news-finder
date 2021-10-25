ea79ce388660e0d22c21fa89d74381ee8c2a5ece
elibrowne <eli.browne.23@bishops.com>
2021-10-25 13:12:19 -0700

    Finished reading in basic data, working on vectorizer

--- findFakeNews.py
+++ findFakeNews.py
@@ -1,17 +1,21 @@
 # IMPORT STATEMENTS
 
 import numpy # calculations, matrices, etc.
-import csv # reading csv files for data intake
+import csv # reading CSV files for data intake
 # Bernoulli Naive-Bayes was the text classification algorithm I was introduced
 # to doing work with the 20newsgroups dataset, so I'm using it here too.
 from sklearn.naive_bayes import BernoulliNB 
-# Vectorizing text is a way to use it to train an algorithm.
+# Vectorizing text is a way to use it to train an algorithm. Count vectorizer 
+# also allows us to manipulate characters and remove stop words!
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 # Classification report and confusion matrix are two ways to display data
 from sklearn.metrics import classification_report, confusion_matrix
 
-# READING IN CSV FILES, ORGANIZING DATA
+""" This code reads in the CSV files containing instances of real and fake news
+and does the most basic things to organize them. It  cleans the data somewhat, 
+removing the date, which isn't important to figuring out if news is real or 
+fake. It also adds an indication of which dataset the entry belongs to. """
 
 # Read in real news files using Python's built-in CSV module
 realNews = [] # will save the content of the real news
@@ -33,7 +37,21 @@ with open('news-sources/fake.csv', mode = 'r') as file:
 		entry["real"] = 0 # the entry is fake so the 'real' datapoint is 0
 		fakeNews.append(entry)
 
+""" This code sets up the vectorizer, which is important for parsing and 
+interpreting the data. The parameters used are as follows: max_features caps
+the amount of considered words (for time/memory purposes), stop_words removes
+commonly-used 'stop-words' that don't contribute to the meaning of the text,
+n-gram range considers how many surrounding words impact a word (not exactly
+but shorthand). """
+
+vectorizer = CountVectorizer(max_features = 100, stop_words = 'english', ngram_range = (1, 3))
+x = vectorizer.fit_transform() # currently working here! I need to transform the text and set up a y axis
+
 # DIVIDING INTO TRAINING AND TEST DATA
+
+# This is probably out of place right now and needs to be done earlier?
+dataset = realNews + fakeNews # combine the two sets: order doesn't matter.
+xTrain, xTest, yTrain, yTest = train_test_split()
 
 # VECTORIZE AND TRAIN ALGORITHM
 
