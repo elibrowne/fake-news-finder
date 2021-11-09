cfde9145b04be1130060dfa6298c9d8b9d77512b
elibrowne <eli.browne.23@bishops.com>
2021-11-08 21:27:35 -0800

    Pickled vectorizer and classifier (files too big to commit)

--- newsSite/classifier/views.py
+++ newsSite/classifier/views.py
@@ -2,27 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.views import View
+from sklearn.feature_extraction.text import CountVectorizer
+import pickle
 
 # Create your views here.
 class index(View):
 	# Only using GET data, no POST data
 	def get(self, request):
 		# Load the template
-		template = loader.get_template('classifier/homepage.html')
+		template = loader.get_template("classifier/homepage.html")
 		articleContent = "" # placeholder for future data 
 		realNews = False
 
-		# See if the user had submitted an article to be judged or not.
-		# Try/catch is used to ignore null errors
-		try:
-			if request.GET['article']:
-				articleContent = request.GET['article']
-		except: 
-			# Adding code to fill the except block
-			print("No article, just loading the webpage.")
+		vectorizer = pickle.load(open("classifier/vectorizer.pkl", 'rb'))
+		articleContent = request.GET['article']
+		print(articleContent)
+		loaded_model = pickle.load(open("classifier/newsClassifier.pkl", 'rb'))
+		print("TRYING")
+		result = loaded_model.predict(vectorizer.transform([articleContent]))[0]
+		print(result)
+		if result == 1:
+			realNews = True
 
 		# Create context: article content to determine if something was entered
 		# and boolean to output real or fake
+		print(realNews)
 		context = {
 			'article': articleContent,
 			'real': realNews
