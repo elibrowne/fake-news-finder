c68926cd3ce666e8eae2f7902d63f4a98e6dd77f
elibrowne <eli.browne.23@bishops.com>
2021-11-08 20:48:37 -0800

    Set up website to take in article inputs

--- newsSite/classifier/views.py
+++ newsSite/classifier/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.views import View
 
 # Create your views here.
-def index(request):
-	template = loader.get_template('classifier/homepage.html')
-	context = {}
-	return HttpResponse(template.render(context, request))
+class index(View):
+	# Only using GET data, no POST data
+	def get(self, request):
+		# Load the template
+		template = loader.get_template('classifier/homepage.html')
+		articleContent = "" # placeholder for future data 
+		realNews = False
+
+		# See if the user had submitted an article to be judged or not.
+		# Try/catch is used to ignore null errors
+		try:
+			if request.GET['article']:
+				articleContent = request.GET['article']
+		except: 
+			# Adding code to fill the except block
+			print("No article, just loading the webpage.")
+
+		# Create context: article content to determine if something was entered
+		# and boolean to output real or fake
+		context = {
+			'article': articleContent,
+			'real': realNews
+		}
+		
+		# Return HTTP response with context
+		return HttpResponse(template.render(context, request))
